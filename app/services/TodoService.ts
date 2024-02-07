bfd0330d3cb9d86d1eb1b22d1fdea62619352315
otgon123 <220054eo@yse-c.net>
2024-02-07 11:45:25 +0900

    push

---
+++ app/services/TodoService.ts
@@ -0,0 +1,40 @@
+import { Todo } from "../models/Todo";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+
+export const getTodos = async () => {
+    //TODO: API URL設定
+    const url = API_URL + "";
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            return await response.json();
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const postTodos = async (todos: Todo[]) => {
+    if (!todos) return;
+    //TODO: API URL設定
+    const url = API_URL + "/todos";
+    //TODO: APIで保存し、データを返す
+    const data = JSON.stringify(todos);
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        });
+
+        if (response.ok) {
+            return await response.json(); // データを返す
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
